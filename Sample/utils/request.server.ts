be64f4ca2bcf5db1be63954569d57d87f02dde74
Nick <nick.latcham@proton.me>
2023-08-31 16:33:50 -0500

    moving files

--- Sample/utils/request.server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from "@remix-run/node";
-
-/**
- * This helper function helps us to renturn the accurate HTTP status,
- * 400 BAD REQUEST, to the client
- */
-
-export const badRequest = <T>(data: T) => json<T>(data, { status: 400 });
